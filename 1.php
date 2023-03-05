71a83a40ca7a132b24b939112b73f49b2af7ac54
Shritej Kumbhar <96112909+shritya@users.noreply.github.com>
2023-03-05 14:54:54 +0530

    Add files via upload

--- 1.php
+++ 1.php
@@ -1,4 +1,218 @@
-<?php include 'header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="fowler.css" />
+    <title>AdiShakti Health Rental Services</title>
+    <link rel="shortcut icon" href="./assets/favicon-16x16.png" type="image/x-icon" />
+  </head>
+  <body>
+    <section class="navbar">
+      <header>
+        <nav>
+          <div class="nav-left">
+            <a href="index.html">
+              <img src="./assets/logo.png" alt="Logo" class="logo" />
+            </a>
+          </div>
+          <!-- <h1>ADISHAKTI</h1> -->
+          <div class="nav-right">
 
-<?php include 'hero.php';?>
-<?php include 'footer.php';?>
+            <ul class="nav-child">
+              <li class="rest-pg"><a href="index.html">Home</a></li>
+              <li class="rest-pg"><a href="ourservice.html">Our Serivce</a></li>
+              <li class="rest-pg"><a href="whoweare.html">Who are we?</a></li>
+              <li class="rest-pg"><a href="product.html">All Products</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </section>
+    <a class="stick" href="https://wa.me/+917887805948"
+        ><img class="stick-img" src="./assets/whatsapp.svg" alt=""
+      /></a>
+    <!-- HEADER END -->
+    <!-- Hero section starts -->
+   
+    <section class="product">
+      <img src="./assets/1.jpg" alt="" class="product-img" />
+      <div class="description">
+        <h2>Muscletech</h2>
+        <p>
+          A manual wheelchair is powered either by the individual using the
+          wheelchair or by an assistant. The most commonly recognized manual
+          wheelchairs are seen at hospitals and nursing homes. Individuals who
+          have the strength and endurance to independently propel the wheelchair
+          typically use manual wheelchairs. They can propel the wheelchair in
+          different ways. For instance, individuals with a spinal cord injury
+          can use their upper extremities. Individuals who have had a stroke
+          that affects only one side of the body can use one upper extremity and
+          one lower extremity. Individuals who have neither the strength to walk
+          without a walker or cane nor the endurance to walk with one can use
+          their lower extremities. An assistant or attendant propels the manual
+          wheelchair when individuals cannot do it themselves.
+        </p>
+      </div>
+    </section>
+    <section class="bill">
+      <h3>Total Expense</h3>
+      <div class="expense">
+        <ul>
+          <li>
+            <h4>Rent</h4>
+          </li>
+          <li>
+            <h4>Deposit</h4>
+          </li>
+          <li>
+            <h4>Delivery Charges</h4>
+          </li>
+          <li><h4>Discount</h4></li>
+        </ul>
+        <ul>
+          <li>
+            <h4>700</h4>
+          </li>
+          <li>
+            <h4>700 X 3 = 2100</h4>
+          </li>
+          <li>
+            <h4>400</h4>
+          </li>
+          <li><h4>-300</h4></li>
+          <li class="line-form">
+            <h4>Total Bill = 2600 Rs.</h4>
+          </li>
+        </ul>
+      </div>
+      <br />
+      <h4>*Note All Products Are Paid Through Cash On Delivery(COD) Method</h4>
+      <br />
+      <section class="form-fill">
+        <button><a class="btn" href="form.html">Book Now</a></button>
+      </section>
+    </section>
+    <section class="hero">
+      <div class="hero-content">
+        <style>
+         
+          </style>
+          </head>
+          <body>
+          <div class="slideshow-container">
+          
+          <div class="mySlides fade">
+            <img class="hero-image" src="./assets/7.jpg">
+            <!-- <div class="text">Caption Text</div> -->
+          </div>
+          
+          <div class="mySlides fade">
+            <img class="hero-image" src="./assets/9.JPG">
+            <!-- <div class="text">Caption Two</div> -->
+          </div>
+          
+          <div class="mySlides fade">
+            <img class="hero-image" src="./assets/2.JPG">
+            <!-- <div class="text">Caption Three</div> -->
+          </div>
+          
+          </div>
+          <br>
+          
+          <div style="text-align:center">
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+          </div>
+          
+          <script>
+          let slideIndex = 0;
+          showSlides();
+          
+          function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+            for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 2 seconds
+          }
+          </script>
+        <!-- <img src="./assets/hero-img.webp" alt="HERO IMAGE" class="hero-img" /> -->
+        <ul class="hero-qualities">
+          <li class="quality-service">
+            <img
+              class="quality-img"
+              src="./assets/bolt_FILL0_wght400_GRAD0_opsz48.svg"
+              alt="S"
+            />Cash On Delivery
+          </li>
+          <li class="quality-service">
+            <img
+              class="quality-img"
+              src="./assets/currency_rupee_FILL0_wght400_GRAD0_opsz48.svg"
+              alt="S"
+            />Clean Equipment
+          </li>
+          <li class="quality-service">
+            <img
+              class="quality-img"
+              src="./assets/fitness_center_FILL0_wght400_GRAD0_opsz48.svg"
+              alt="S"
+            />Low Deposit
+          </li>
+          <li class="quality-service">
+            <img
+              class="quality-img"
+              src="./assets/sprint_FILL0_wght400_GRAD0_opsz48.svg"
+              alt="S"
+            />Low Rent
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="end">
+      <footer>
+        <a href="index.html">
+          <img src="./assets/logo.png" alt="Logo" class="logo" />
+        </a>
+        <div class="adi-footer">
+          <section class="link-footer">
+            <div class="links">
+              <h2>Our Links</h2>
+              <ul class="links-footer">
+                <li>Instagram</li>
+                <li>Whatsapp</li>
+                <li>Facebook</li>
+                <li>Linkedin</li>
+              </ul>
+            </div>
+            <div class="privacy-policy">
+              <h2>Our Policy</h2>
+              <ul class="policy">
+                <li>Privacy</li>
+                <li>Policy</li>
+              </ul>
+            </div>
+          </section>
+          <h4 class="logo-footer-txt">
+            ADISHAKTI | &#169;
+            <span class="footer-contact"
+              >Contact&nbsp;:&nbsp;+91&nbsp;7887805948 +91&nbsp;7378837011</span
+            >
+          </h4>
+        </div>
+      </footer>
+    </section>
+  </body>
+</html>
